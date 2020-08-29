bd38f24f9a33d49e951a19f996a5fd51e17e460e
Yoan Ivanov <yoan.ivanov13@gmail.com>
2020-08-29 13:47:49 +0300

    add

---
+++ Objects and Classes/Tickets.js
@@ -0,0 +1,27 @@
+function solve(tickets, criteria) {
+    class Ticket {
+        constructor(descriptor) {
+            const [destination, price, status] = descriptor.split('|')
+            this.destination = destination;
+            this.price = Number(price);
+            this.status = status;
+        }
+    }
+
+    return tickets.map(t => new Ticket(t)).sort(comparator);
+
+    function comparator(a, b) {
+        try {
+            return a[criteria].localeCompare(b[criteria])
+        } catch (e) {
+            return a[criteria] - b[criteria];
+        }
+    }
+}
+
+console.log(solve(['Philadelphia|94.20|available',
+ 'New York City|95.99|available',
+ 'New York City|95.99|sold',
+ 'Boston|126.20|departed'],
+'destination'
+));
